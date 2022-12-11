a6ba5534c53193cd96b8af9bdd43492099d86170
Timon Marweg <timon.marweg@student.fh-kiel.de>
2022-12-11 19:18:03 +0100

    fixed a1

--- week7/Aufgabe 1.hs
+++ week7/Aufgabe 1.hs
@@ -2,15 +2,15 @@ data Tree n l = Leaf l | Node (Tree n l) n (Tree n l)
   deriving Show
 
 --Definition der Instanzen
-instance Functor Tree where
+instance Functor (Tree n) where
   fmap f (Leaf x)     = Leaf (f x)
   fmap f (Node l n r ) = Node (fmap f l) n (fmap f r)
 
-instance Applicative Tree where
+instance Applicative (Tree n) where
   pure x = Leaf x
-  (Leaf f) <*> t      = fmap f tails
+  (Leaf f) <*> t      = fmap f t
   (Node l n r) <*> t  = Node (l <*> t) n (r <*> t)
 
-instance Monad Tree where
+instance Monad (Tree n) where
   (Leaf x) >>= f = f x
   (Node l n r) >>= f = Node (l >>= f) n (r >>= f)
