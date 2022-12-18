50a2f2e4ba005f892899d89cf7df8315faa487d0
Sven Niedorf <111495729+SvenNied@users.noreply.github.com>
2022-12-18 22:06:21 +0100

    Update Aufgabe3.pl

--- week8/Aufgabe3.pl
+++ week8/Aufgabe3.pl
@@ -14,3 +14,46 @@ not(false,true).
 ex1(X,Y,Z,Result)     :- and(X,Y,Value), or(Value, Z, Result).
 ex2(X,Y,Z,Result)    :- and(X,Y,Value1), and(Y,Z,Value2), or(Value2, Z, Value3), or(Value1, Value3, Result).
 ex3(X,Y,Z,Result)        :- not(Y,Value1), and(X,Value1,Value2), and(Value2,Z,Value3), and(Z,Y,Value4), or(Value4,Z,Value5), or(Value3,Value5, Result).
+
+Wir stellen folgende Anfragen:
+?- ex1(true,false,true,Result).
+Result = true.
+
+?- ex2(true,false,true,Result).
+Result = true.
+
+?- ex3(true,false,true,Result).
+Result = true.
+
+Wie ist die Belegung um true zu erhalten?
+?- ex1(X,Y,Z,true).
+X = Y, Y = true ;
+X = Z, Z = true,
+Y = false ;
+X = false,
+Y = Z, Z = true ;
+X = Y, Y = false,
+Z = true.
+
+
+?- ex2(X,Y,Z,true).
+X = Y, Y = Z, Z = true ;
+X = Y, Y = true,
+Z = false ;
+X = Z, Z = true,
+Y = false ;
+X = false,
+Y = Z, Z = true ;
+X = Y, Y = false,
+Z = true .
+
+?- ex3(X,Y,Z,true).
+X = Y, Y = Z, Z = true ;
+X = false,
+Y = Z, Z = true ;
+X = Z, Z = true,
+Y = false ;
+X = Y, Y = false,
+Z = true .
+
+
