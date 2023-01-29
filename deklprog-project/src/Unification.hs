75f694603641fd4f8b4b707bebd4f0b8658d0b6c
Sven Niedorf <111495729+SvenNied@users.noreply.github.com>
2023-01-29 21:31:22 +0100

    Update Unification.hs

--- deklprog-project/src/Unification.hs
+++ deklprog-project/src/Unification.hs
@@ -2,7 +2,7 @@
 
 module Unification
   ( testUnification
-  , -- unify
+  , ds , unifiy
   ) where
 
 
@@ -11,8 +11,42 @@ import Data.Maybe
 import Test.QuickCheck
 
 -- Properties
+ds :: Term -> Term -> Maybe (Term, Term)
+ds (Var x)     (Var y)     | x == "_"  = Nothing
+                           | y == "_"  = Nothing
+                           | x == y    = Nothing
+                           | otherwise = Just (Var x, Var y)
+ds (Var x)     y           | x == "_"  = Nothing
+                           | otherwise = Just (Var x, y)
+ds x           (Var y)     | y == "_"  = Nothing          
+                           | otherwise = Just (Var y, x)
 
-{- Uncomment this to test the properties when all required functions are implemented
+ds (Comb f ft) (Comb g gt)            = if(f /= g || (length ft /= length gt)) 
+                                          then Just ((Comb f ft), (Comb g gt))
+                                                 else firstDsagInList ft gt  
+  where                
+    firstDsagInList :: [Term] -> [Term] -> Maybe(Term, Term)
+    firstDsagInList [] [] = Nothing
+    firstDsagInList (term1:terms1) (term2:terms2) = if (ds a b == Nothing) 
+                                                      then firstDsagInList as bs
+                                                      else ds a b                       
+
+-- determines the mgu
+unify :: Term -> Term -> Maybe Subst
+unifiy term1 term2 = undefined
+
+-- determines the most general unifier (mgu)
+    unify :: Term -> Term -> Maybe Subst
+    unify term1 term2 = (mgu term1 term2) empty
+        where
+            mgu subst term1 term2 = if ds (apply subst term1) (apply subst term2) == Just (Var varname, someTerm) || ds (apply subst term1) (apply subst term2) == Just (someTerm, Var varname)
+                                      then if (varname 'elem' allVars someTerm) then Nothing else mgu (compose (single varname someTerm) subst) term1 term2  
+                                        else if (ds (apply subst term1) (apply subst term2) == Just _ ) then Nothing
+                                          else if (ds (apply subst term1) (apply subst term2) == Nothing) then Just subst
+
+{- 
+
+Uncomment this to test the properties when all required functions are implemented
 
 -- Does  a variable occur in a term?
 occurs :: VarName -> Term -> Bool
