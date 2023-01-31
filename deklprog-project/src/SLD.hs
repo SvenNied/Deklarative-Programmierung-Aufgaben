ced312459ee8ffd923a85545d9f3c6d7504de2a2
Timon Marweg <timon.marweg@student.fh-kiel.de>
2023-01-31 23:54:19 +0100

    bfs, dfs

--- deklprog-project/src/SLD.hs
+++ deklprog-project/src/SLD.hs
@@ -1,16 +1,17 @@
 module SLD
   ( SLDTree (..),
     sld,
-    -- Strategy,
-    -- dfs,
-    -- bfs,
-    -- solveWith,
+    Strategy,
+    dfs,
+    bfs,
+    solveWith,
   )
 where
 
 import Base.Type
 import Subst
 import Unification
+import Data.List
 import Data.Maybe (catMaybes)
 
 -- Data type for an SLD tree
@@ -30,10 +31,18 @@ sld prog@(Prog rules) goal@(Goal (firstTerm:_))  = SLDTree goal (catMaybes (map 
     applySubst (Just subst) term = Just (subst, sld prog (Goal [(apply subst term)]))
 
 dfs :: Strategy
-dfs = undefined
+dfs (SLDTree _ []) = []
+dfs (SLDTree _ nodes) = concatMap handleNode nodes
+  where
+    handleNode:: (Subst,SLDTree) -> [Subst]
+    handleNode (subst, tree) = map (compose subst) (dfs tree)
 
 bfs :: Strategy
-bfs = undefined
+bfs (SLDTree _ []) = []
+bfs (SLDTree _ nodes) = map (\(subst,_)-> subst) (filter nodeWithEmptyTree nodes) ++ concatMap bfs (map (\(_,tree) -> tree) (filter (not . nodeWithEmptyTree) nodes))
+  where
+    nodeWithEmptyTree (_,(SLDTree _ [])) = True
+    nodeWithEmptyTree (_,(SLDTree _ _)) = False
 
 solveWith :: Prog -> Goal -> Strategy -> [Subst]
 solveWith prog goal strat = strat (sld prog goal)
